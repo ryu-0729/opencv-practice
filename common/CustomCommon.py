71ce41d836d7bfe625b4a511111026a2fc9ffbba
1844ryudai@gmail.com <1844ryudai>
2022-11-06 16:32:24 +0900

    feat: Processing Commonality

---
+++ common/CustomCommon.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+import cv2
+
+
+@dataclass
+class CustomCommon:
+    __filePath: str
+    __downloadPath: str
+    __grayImgPath: str = ''
+
+    @property
+    def filePath(self):
+        return self.__filePath
+
+    @property
+    def downloadPath(self):
+        return self.__downloadPath
+
+    @property
+    def grayImgPath(self):
+        return self.__grayImgPath
+
+    def imgRead(self):
+        """ 画像の読み込み """
+        return cv2.imread(self.__filePath)
+
+    def grayScale(self, img):
+        """ 白と黒でグレースケールした画像を返却 """
+        grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if self.__grayImgPath:
+            cv2.imwrite(self.__grayImgPath, grayImg)
+        return grayImg
+
+    def imgDownload(self, img):
+        """ 指定したパスに画像を出力 """
+        return cv2.imwrite(self.__downloadPath, img)
+
+    def faceDetection(self, grayImg):
+        """ 顔検出処理 """
+        faceCascade = cv2.CascadeClassifier(
+            '/usr/local/opt/opencv/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
+        )
+        return faceCascade.detectMultiScale(grayImg)
+
+    def multipleRectangles(self, rectanglesImg, imgData):
+        """ 複数の対象を四角で囲み出力する処理 """
+        for (x, y, w, h) in imgData:
+            rectanglesImg = cv2.rectangle(rectanglesImg, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        return cv2.imwrite(self.__downloadPath, rectanglesImg)
--- faceDetectionSample.py
+++ faceDetectionSample.py
@@ -1,20 +1,10 @@
-import cv2
+import sys
+sys.path.append('./common/CustomCommon.py')
+import common.CustomCommon as customCommon
 
-# NOTE: 顔検出したい画像の読み込み
-faceImg = cv2.imread('./sample-face.png')
+custom = customCommon.CustomCommon('./sample-face.png', './face.jpg', './grayFaceImg.jpg')
 
-# NOTE: 白と黒のグレースケール
-grayFaceImg = cv2.cvtColor(faceImg, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('./grayFaceImg.jpg', grayFaceImg)
-
-# NOTE: 顔検出処理
-faceCascade = cv2.CascadeClassifier(
-  '/usr/local/opt/opencv/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
-)
-faceData = faceCascade.detectMultiScale(grayFaceImg)
-
-# NOTE: 顔を四角で囲む処理
-for (x, y, w, h) in faceData:
-  faceImg = cv2.rectangle(faceImg, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-cv2.imwrite('./face.jpg', faceImg)
+faceImg = custom.imgRead()
+grayFaceImg = custom.grayScale(faceImg)
+faceData = custom.faceDetection(grayFaceImg)
+custom.multipleRectangles(faceImg, faceData)
